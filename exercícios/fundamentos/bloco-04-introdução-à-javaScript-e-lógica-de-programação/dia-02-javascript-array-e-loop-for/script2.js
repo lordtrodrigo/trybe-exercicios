5493d56c039b56deb522f34ed3654ab4db6053e3
Thiago Viana <trodrigo734@gmail.com>
2022-04-07 15:53:24 -0300

    Resolve os exercícios de for.

---
+++ exercícios/fundamentos/bloco-04-introdução-à-javaScript-e-lógica-de-programação/dia-02-javascript-array-e-loop-for/script2.js
@@ -0,0 +1,26 @@
+/*  EXERCÍCIO 01
+    APRESENTAR AS MARCAS DE CARRO CONTIDAS NA LISTA UTILIZANDO O FOR
+*/
+
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+// console.log(cars[0]); 
+// console.log(cars[1]); 
+// console.log(cars[2]); 
+
+for (index = 0; index < cars.length; index += 1) {
+    console.log(cars[index]);
+}
+
+
+
+/*  EXERCÍCIO 02
+    Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+*/
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
