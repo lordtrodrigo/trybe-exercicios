2232879586ecbadd7c92b44462f1482ba5eaad96
Thiago Viana <trodrigo734@gmail.com>
2022-04-13 19:05:47 -0300

    resolve a quatão 05 sobre DOM

--- exercícios/fundamentos/bloco-05-javascript-dom-e-seletores/script.js
+++ exercícios/fundamentos/bloco-05-javascript-dom-e-seletores/script.js
@@ -17,23 +17,49 @@
 
         // 2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109).
 
-        function changeBackground(color) {
+        function changeBackground1(color) {
             document.getElementsByClassName('main-content')[0].style.backgroundColor = color;
         }
 
-        changeBackground('rgb(76,164,109)');
+        changeBackground1('rgb(76,164,109)');
+
 
         // 3. Crie uma função que mude a cor do quadrado vermelho para branco.
 
+        function changeBackground2(color) {
+            document.querySelectorAll('section','.center-content')[0].style.backgroundColor = color;
+        }
+
+        changeBackground2('white');
 
 
-
-
-
-
-        
         // 4. Crie uma função que corrija o texto da tag <h1>.
 
+        function correctText(newText) {
+            document.getElementsByTagName('h1')[0].innerText = newText;
+        }
+        correctText('Exercício 5.1 - JavaScript');
+
+
         // 5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+        
+        function upperCase() {
+            // let text = '';
+            // for (let index = 0; index < document.getElementsByTagName('p').length; index += 0) {
+            //     text = document.getElementsByTagName('p')[index].innerText;
+            //     text = text.toUpperCase;
+            // }
             
+            // let text = document.getElementsByTagName('p')[0].innerText;
+            // text = text.toUpperCase();
+
+            document.getElementsByTagName('p')[0].innerText = document.getElementsByTagName('p')[0].innerText.toUpperCase();
+        
+
+        }
+
+        upperCase();
+
+        // let text = document.getElementsByTagName('p')[0].innerText;
+
         // 6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
