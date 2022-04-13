453efa5e8753b55559d5d271e541b975ce817e12
Thiago Viana <trodrigo734@gmail.com>
2022-04-13 18:33:19 -0300

    resolve os exercícios 1 e 2 sobre o DOM

---
+++ exercícios/fundamentos/bloco-05-javascript-dom-e-seletores/script.js
@@ -0,0 +1,39 @@
+
+        // Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+        // - document.getElementById()
+        // - document.getElementsByClassName()
+        // - document.getElementsByTagName()
+        // - document.querySelector()
+        // - document.querySelectorAll()
+
+        // 1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
+        function changeText(newText) {
+            document.querySelector('p').innerText = newText;
+        }
+
+        changeText('Em dois anos me vejo atuando no mercado como desenvolvedor pleno.');
+
+
+        // 2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109).
+
+        function changeBackground(color) {
+            document.getElementsByClassName('main-content')[0].style.backgroundColor = color;
+        }
+
+        changeBackground('rgb(76,164,109)');
+
+        // 3. Crie uma função que mude a cor do quadrado vermelho para branco.
+
+
+
+
+
+
+
+        
+        // 4. Crie uma função que corrija o texto da tag <h1>.
+
+        // 5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+            
+        // 6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
