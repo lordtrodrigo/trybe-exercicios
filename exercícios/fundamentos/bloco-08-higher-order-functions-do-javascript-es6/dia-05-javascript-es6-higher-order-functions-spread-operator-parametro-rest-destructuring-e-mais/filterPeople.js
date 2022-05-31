093a9578156d0729a174dc93e8405186f5bd7f20
Thiago Viana <trodrigo734@gmail.com>
2022-05-31 15:14:33 -0300

    Start front end

--- exercícios/fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-higher-order-functions-spread-operator-parametro-rest-destructuring-e-mais/filterPeople.js
+++ exercícios/fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-higher-order-functions-spread-operator-parametro-rest-destructuring-e-mais/filterPeople.js
@@ -2,33 +2,46 @@
 //          Dica: use object destructuring.
 
 const people = [
-    {
-      name: 'Nicole',
-      bornIn: 1992,
-      nationality: 'Australian',
-    },
-    {
-      name: 'Harry',
-      bornIn: 2008,
-      nationality: 'Australian',
-    },
-    {
-      name: 'Toby',
-      bornIn: 1901,
-      nationality: 'Australian',
-    },
-    {
-      name: 'Frida',
-      bornIn: 1960,
-      nationality: 'Dannish',
-    },
-    {
-      name: 'Fernando',
-      bornIn: 2001,
-      nationality: 'Brazilian',
-    },
-  ];
-  
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+//🚀 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+//   Dica: use object destructuring.
 
 
-  module.exports = { filterPeople, people };
+const filterPeople = (array) => {
+  return array.filter((item) => {
+          const { bornIn, nationality } = item;
+          return bornIn < 2001 && nationality === 'Australian';
+        });
+}
+
+
+
+console.log(filterPeople(people));
+
+module.exports = { filterPeople, people };
